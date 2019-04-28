64dbb04c924222667c4d4cf99580008f04120055
haoyanyu <1281653707@qq.com>
2019-04-28 14:17:36 +0800

    修改图片引入方法为import 。 from ‘../../';

--- public/src/pages/components/Box/index.js
+++ public/src/pages/components/Box/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './index';
+import logo from './../../../images/logo.png';
 
 class BoxIndex extends React.Component{
 	render(){
 		return (
 			<div>
-				box
+				<div className="addBox">
+					<p>
+						<img src={logo} />
+						<span>添加盒子</span>
+					</p>
+				</div>
 			</div>
 		)
 	}
